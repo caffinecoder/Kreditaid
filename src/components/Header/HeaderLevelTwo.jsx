0fb5c22931a75d137c00b439d311c00c7029217f
manoj mandal <hackintown99@gmail.com>
2024-02-09 16:12:17 +0530

    components updated

--- src/components/Header/HeaderLevelTwo.jsx
+++ src/components/Header/HeaderLevelTwo.jsx
@@ -1,39 +1,10 @@
 import React from "react";
-import ReactFlagsSelect from "react-flags-select";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPhoneVolume, fas } from "@fortawesome/free-solid-svg-icons";
-import { faBell } from "@fortawesome/free-solid-svg-icons";
-import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import Styles from "./HeaderLvlTwo.module.css";
-import { useState } from "react";
+import CompanySearch from "../SearchBox/CompanySearch";
 
 const HeaderLevelTwo = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [companyName, setCompanyName] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const handleSearch = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(
-        `http://206.189.135.190/api/get-company-list-by-name-or-other-keywords/${searchTerm}`
-      );
-      const data = await response.json();
-      setCompanyName(data.companyList);
-    } catch (error) {
-      console.error("Error fetching company data:", error);
-      setCompanyName("Company not found");
-    }
-    setLoading(false);
-  };
-  const handleKeyPress = (event) => {
-    if (event.key === "Enter") {
-      handleSearch();
-    }
-  };
-  const handleInputChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
   return (
     <div>
       <div className={Styles["header-level-2"]}>
@@ -49,50 +20,17 @@ const HeaderLevelTwo = () => {
           </div>
           <div className={Styles["level-2-right"]}>
             <div className={Styles["comp-search"]}>
-              <div className={Styles["search-wrap"]}>
-                <input
-                  type="text"
-                  className={Styles["comp-search-input"]}
-                  placeholder="Search for the company"
-                  value={searchTerm}
-                  onChange={handleInputChange}
-                  onKeyDown={handleKeyPress}
-                ></input>
-                <div className={Styles["search-whole"]}>
-                  <button
-                    className={Styles["search-btn"]}
-                    onClick={handleSearch}
-                  >
-                    <FontAwesomeIcon icon={faMagnifyingGlass} />
-                  </button>
-                </div>
-                <div className={Styles["mic-whole"]}>
-                  <FontAwesomeIcon icon={faMicrophone} />
-                </div>
-              </div>
+              <CompanySearch />
               <div className={Styles["login-dropdown"]}>
                 <button className={Styles["loginBtn"]}>Login</button>
               </div>
-              <div className={Styles["notification"]}>
-                <FontAwesomeIcon icon={faBell} />
-                <span className={Styles["notification-count"]}>20</span>
+              <div className={Styles["mycart"]}>
+                <FontAwesomeIcon icon={faCartShopping} />
+                <span className={Styles["mycart-count"]}>20</span>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className={Styles["company-info"]}>
-        {loading && <div>Loading...</div>}
-        {!loading &&
-          companyName &&
-          companyName.map((item, index) => {
-            const {companyName, companyID}  = item
-            return (
-              <ul key={index}>
-                <li>{companyName}</li>
-              </ul>
-            );
-          })}
       </div>
     </div>
   );
---
+++ src/components/SearchBox/CompanySearch.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import ReactFlagsSelect from "react-flags-select";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faL, faPhoneVolume, fas } from "@fortawesome/free-solid-svg-icons";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
+import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import Styles from "./CompanySearch.module.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const CompanySearch = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [companyName, setCompanyName] = useState([]);
+  const [showResults, setShowResults] = useState(false);
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(
+        `http://206.189.135.190/api/get-company-list-by-name-or-other-keywords/${searchTerm}`
+      );
+      const data = await response.json();
+      setCompanyName(data.companyList);
+      setShowResults(true);
+      if (setShowResults(true)) {
+        alert("error");
+      }
+    } catch (error) {
+      console.error("Error fetching company data:", error);
+      setCompanyName("Company not found");
+    }
+  };
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+    setShowResults(false);
+  };
+  useEffect(() => {
+    if (searchTerm.length > 0) {
+      handleSearch();
+    } else {
+      setCompanyName([]);
+      setShowResults(false);
+    }
+  }, [searchTerm]);
+  return (
+    <div className={Styles["search-wrap"]}>
+      <input
+        type="text"
+        className={Styles["comp-search-input"]}
+        placeholder="Search for the company"
+        value={searchTerm}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyPress}
+      />
+      <div className={Styles["search-whole"]}>
+        <button className={Styles["search-btn"]} onClick={handleSearch}>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </button>
+      </div>
+      <div className={Styles["mic-whole"]}>
+        <FontAwesomeIcon icon={faMicrophone} />
+      </div>
+      <div
+        className={Styles["comp-info-wrap"]}
+        style={{ display: showResults ? "block" : "none" }}
+      >
+        <ul className={Styles["comp-details-listing"]}>
+          {companyName.map((item, index) => {
+            const { companyName, companyID } = item;
+            return (
+              <li key={index} className={Styles["comp-details-list"]}>
+                <Link to={`/company/${companyID}`}>{companyName}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div>
+        {companyName.success === false && (
+          <div>{companyName.Error.message}</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CompanySearch;
