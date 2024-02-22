c4059df812a505e57f60b5ee46f6caa18728f738
manoj mandal <hackintown99@gmail.com>
2024-02-22 12:01:37 +0530

    Login-Page-Done

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from "react";
+import Styles from "./Signup.module.css";
 
 const Signup = () => {
   return (
     <div>
-      hi Iam sign Up Page
+      <div className={Styles["signup-form-wrap"]}>
+        <div className={Styles["signup-container"]}>
+          <div className={Styles["signup-left"]}>
+            <img src="signup-image.jpg" alt="signup-image" />
+          </div>
+          <div className={Styles["signup-right"]}>
+            <form className={Styles["form"]}>
+              <div className={Styles["form-container"]}>
+                <p className={Styles.title}>Register</p>
+                <div className={Styles["input-group"]}>
+                  <label htmlFor="username">Full Name</label>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="Email"
+                  />
+                </div>
+                <div className={Styles["input-group"]}>
+                  <label htmlFor="username">Email</label>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="Email"
+                  />
+                </div>
+                <div className={Styles["input-group"]}>
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
